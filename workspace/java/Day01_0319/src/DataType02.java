98f97ac3250c65a1071075fa4045ab539d79bd9a
DragonCastle7512 <155559674+DragonCastle7512@users.noreply.github.com>
2025-05-21 20:48:30 +0900

    Add files via upload

---
+++ workspace/java/Day01_0319/src/DataType02.java
@@ -0,0 +1,10 @@
+
+public class DataType02 {
+	public static void main(String[] args) {
+		int age;
+		age = 25;
+		System.out.println("나이: "+age);
+		age = 30;
+		System.out.println("나이: "+age);
+	}
+}
